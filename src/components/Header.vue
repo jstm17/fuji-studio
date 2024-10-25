ff0d083234629a34641c8023c6269f9649e956fc
fideline <fideline.bazana-ntomo@charlie-solutions.com>
2024-10-25 13:07:44 +0200

    fix conflits

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,13 @@
 <template>
     <header class="bounded flex justify-between pt-20 mb-10">
         <p class="logo">FUJI STUDIO</p>
-        <nav class="flex gap-20">
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">À propos</router-link>
-      <router-link to="/portfolio">Portfolio</router-link>
-      <!-- <router-link to="/services">Services</router-link>
-      <router-link to="/contact">Contact</router-link> -->
+        <nav>
+            <ul class="flex gap-6">
+                <li>A propos</li>
+                <li>Portfolio</li>
+                <li>Services</li>
+                <li>Contact</li>
+            </ul>
         </nav>
     </header>
 </template>
